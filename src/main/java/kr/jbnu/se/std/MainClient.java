454bfdc41ab78b9ac6c4590b115ce1bd9fb01075
kimjaehoug <150125391+kimjaehoug@users.noreply.github.com>
2024-10-02 18:59:33 +0900

    131313

--- src/main/java/kr/jbnu/se/std/MainClient.java
+++ src/main/java/kr/jbnu/se/std/MainClient.java
@@ -1,4 +1,18 @@
 package kr.jbnu.se.std;
 
-public class MainClient {
+import javax.swing.*;
+
+public class MainClient extends JFrame {
+    private JPanel mainpanel;
+    private JButton button1;
+
+    public MainClient() {
+        setTitle("Shoot the Dock");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1280,720);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+        setContentPane(mainpanel);
+    }
 }
