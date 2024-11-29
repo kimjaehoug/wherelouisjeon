61614be428682af5a3b0c51365a2d0483482a91a
kingyewon <helloalal23@naver.com>
2024-11-30 00:08:36 +0900

    import java.awt.event.ActionEvent; import java.awt.event.ActionListener; //unused라 지움//

--- src/main/java/kr/jbnu/se/std/Framework.java
+++ src/main/java/kr/jbnu/se/std/Framework.java
@@ -17,8 +17,6 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
